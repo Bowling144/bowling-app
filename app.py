1c0632900ccc65995f151d27717c42b1db859180
Bowling144 <tanaka0531@yahoo.co.jp>
2026-03-18 00:29:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -217,7 +217,7 @@ if app_mode == "📊 プレイヤー分析":
     </div>
   </div>
   <div style="text-align: center; margin-top: -45px; position: relative; z-index: 10; filter: drop-shadow(0 10px 10px rgba(0,0,0,0.8));">
-    <div style="display: inline-block; width: 25%; background: linear-gradient(135deg, #bf953f 0%, #fcf6ba 25%, #b38728 50%, #fbf5b7 75%, #aa771c 100%); padding: 12px 0px 20px 0px; clip-path: polygon(0% 0%, 100% 0%, 100% 75%, 50% 100%, 0% 75%); border-radius: 2px;">
+    <div style="display: inline-block; width: 21%; background: linear-gradient(135deg, #bf953f 0%, #fcf6ba 25%, #b38728 50%, #fbf5b7 75%, #aa771c 100%); padding: 12px 0px 20px 0px; clip-path: polygon(0% 0%, 100% 0%, 100% 75%, 50% 100%, 0% 75%); border-radius: 2px;">
       <span style="color: #1a1a1c; font-size: 40px; font-weight: 900; font-family: 'Arial Black', Impact, sans-serif; text-shadow: 1px 1px 0px #fff; display: inline-block; letter-spacing: 2px;">{flight_short}</span>
     </div>
   </div>
