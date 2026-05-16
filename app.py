753d6023aeff36a800e7398dfa4b627e4957698b
Bowling144 <tanaka0531@yahoo.co.jp>
2026-05-16 19:52:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -57,12 +57,12 @@ from PIL import Image
 import os
 
 # アップロードしたファイル名に合わせて拡張子（.jpg または .png）を変更してください
-icon_path = "icon.jpg" 
+icon_path = "icon2.png"
 
 if os.path.exists(icon_path):
-    app_icon_img = Image.open(icon_path)
+    app_icon_img = Image.open(icon_path)
 else:
-    app_icon_img = "🎳" # 画像が見つからない場合の予備
+    app_icon_img = "🎳" # 画像が見つからない場合の予備
 
 st.set_page_config(page_title="ボウリング解析", page_icon=app_icon_img, layout="wide")
 
